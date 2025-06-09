21b6d6e1bb6fa0df03b97d92b649535c16bbcec8
Anatolii Kryvda <anatolii.kryvda@gmail.com>
2025-06-10 02:11:57 +0300

    OpenAI chat

--- chat-openai.py
+++ chat-openai.py
@@ -1,15 +1,15 @@
 from openai import OpenAI
+from dotenv import load_dotenv
 import time
 
 start = time.time()
 
-ollama = OpenAI(base_url="http://localhost:11434/v1", api_key="dummy")
-
+load_dotenv(override=True)
+client = OpenAI()
 messages = [{"role": "user", "content": "Who are you?"}]
 # Ollama should be running, see readme file
-response = ollama.chat.completions.create(model="llama3.2", messages=messages)
+response = client.chat.completions.create(model="gpt-4.1-nano", messages=messages)
 answer = response.choices[0].message.content
-
 print(answer)
 
 end = time.time()
