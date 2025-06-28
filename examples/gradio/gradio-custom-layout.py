a6f316def00ae1cbb1d3e76847a8c1217a2bce37
Anatolii Kryvda <anatolii.kryvda@gmail.com>
2025-06-28 19:55:43 +0300

    Gradio simple example

--- examples/gradio/gradio-custom-layout.py
+++ examples/gradio/gradio-custom-layout.py
@@ -4,37 +4,40 @@ import string
 import gradio as gr
 
 
-def generate_random_value(value_type):
-    if value_type == "integer":
-        return random.randint(0, int(1e9))
-    elif value_type == "string":
-        return ''.join(random.choices(string.ascii_letters, k=10))
-    else:
-        return f"Select value type"
+def random_string(max_length):
+    if not max_length:
+        return "Enter max length"
+    try:
+        length = int(max_length)
+        if length <= 0:
+            return "Enter positive value"
+        return "".join(random.choices(string.ascii_letters, k=int(max_length)))
+    except ValueError:
+        return "Enter integer value"
 
 
 async def reset():
     return "", None
 
 
-async def process_message(value_type, history):
+async def generate(max_length, history):
     # Here we could call LLM
     return history + [
-        {"role": "user", "content": f"Generate random {value_type}"},
-        {"role": "assistant", "content": str(generate_random_value(value_type))}]
+        {"role": "user", "content": f"Generate a random string of length {max_length}"},
+        {"role": "assistant", "content": random_string(max_length)}]
 
 
 with gr.Blocks(theme=gr.themes.Default(primary_hue="emerald")) as demo:
-    gr.Markdown("## Random value generator")
+    gr.Markdown("## Random string generator")
 
     with gr.Row():
         chatbot = gr.Chatbot(label="History", height=300, type="messages")
     with gr.Row():
-        value_type = gr.Dropdown(["", "integer", "string"], label="Value type")
+        max_length = gr.Textbox(show_label=False, placeholder="Max length")
     with gr.Row():
         reset_button = gr.Button("Reset", variant="stop")
-        submit_button = gr.Button("Generate", variant="primary")
-    reset_button.click(reset, [], [value_type, chatbot])
-    submit_button.click(process_message, [value_type, chatbot], [chatbot])
+        generate_button = gr.Button("Generate", variant="primary")
+    reset_button.click(reset, [], [max_length, chatbot])
+    generate_button.click(generate, [max_length, chatbot], [chatbot])
 
 demo.launch()
